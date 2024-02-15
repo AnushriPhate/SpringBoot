377f588d440a3065aef4e9dd1639bd68f7526190
AnushriPhate <anushrisphate@gmail.com>
2024-02-15 13:43:49 +0530

    Resolve Ambiguity @Primary @ComponentScan Annotations Removing Bean conflict using @Qualifier

--- SpringCoreConcepts/SpringCoreConcepts/src/main/java/com/Spring/core/couple/Cat.java
+++ SpringCoreConcepts/SpringCoreConcepts/src/main/java/com/Spring/core/couple/Cat.java
@@ -1,10 +1,13 @@
 package com.Spring.core.couple;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+//@Component("cat") // if dont wanna use it like this we can also use @Qualifier here
 @Component
-@Primary
+@Qualifier("cat")
+//@Primary
 public class Cat implements Animal{
     @Override
     public void play() {
